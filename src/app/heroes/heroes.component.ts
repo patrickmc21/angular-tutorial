adcec84a972ed84065a0ea3c15c4fd4b35c579be
Patrick McLaughlin <patrickmc21@gmail.com>
2018-06-15 17:10:59 -0600

    Style components, finish hero detail routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,12 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  heroes: Hero[];  
 
   getHeroes(): void {
     this.heroService.getHeroes()
